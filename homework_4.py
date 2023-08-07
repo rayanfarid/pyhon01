46d25ba33385bccc7d3089986a6ddb7ac27bd93b
Rayan <rayan.PlayStation1@gmail.com>
2023-08-07 10:58:24 +0300

    hw4

--- homework_4.py
+++ homework_4.py
@@ -1,11 +1,34 @@
 import turtle
-select=int(input("select your shape\n 1=circle 2= square: "))
+import random
+select=int(input("select your shape\n 1.circle \n2.square \n3.triangle \n4.random shape\nEnter here: "))
 radius = int(input("radius: "))
+color=input("Enter color shape: ")
 t = turtle.Turtle()
+def s1():
+    t.circle(radius)
+    t.color(color)
+def s2():
+    for f in range(4):
+         t.forward(radius)
+         t.left(90)
+         t.color(color)
+def s3():
+    t.forward(radius)
+    t.left(90)
+    t.forward(radius)
+    t.left(90)
+    t.forward(radius+radius/2)
+def s4():
+    randoms=random.choice(s1(),s2(),s3())
+    print(randoms)
+
 if select==1:
     t.circle(radius)
 elif select==2:
-    t.forward(radius)
-    t.left(90)
+   s2()
+elif select==3:
+    s3()
+elif select==4:
+    s4()
 else:
-    print("SentaxeRror")
+    print("SentaxError")
